306ecc0f2ef6f8eec564863547a5e3cef72f3eda
Isabela <isabela.franca.lima@gmail.com>
2025-11-17 15:40:37 -0300

    implementar classe TudoMaiuscula usando Template Method

--- TudoMaiuscula.java
+++ TudoMaiuscula.java
@@ -1,3 +1,13 @@
-public class TudoMaiuscula {
-    
+public class TudoMaiuscula extends ProcessadorTexto {
+
+    @Override
+    protected char[] converterCaracteres(char[] chars) {
+        char[] resultado = new char[chars.length];
+
+        for(int i = 0; i < chars.length; i++) {
+            resultado[i] = Character.toUpperCase(chars[i]);
+        }
+
+        return resultado;
+    }
 }
