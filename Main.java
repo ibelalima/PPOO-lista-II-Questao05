7079f538af1751a90b867d4fb1d064a27a632de3
Isabela <isabela.franca.lima@gmail.com>
2025-11-17 15:47:33 -0300

    adicionar classe Main para teste

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        String texto = "Olá Mundo! Template Method FUNCIONANDO.";
+
+        ProcessadorTexto p1 = new TudoMaiuscula();
+        ProcessadorTexto p2 = new TudoMinuscula();
+
+        String resultadoMaiusculo = p1.processar(texto);
+        String resultadoMinusculo = p2.processar(texto);
+
+        System.out.println("Texto original: " + texto);
+        System.out.println("Tudo Maiúsculo: " + resultadoMaiusculo);
+        System.out.println("Tudo Minúsculo: " + resultadoMinusculo);
+    }
 }
+
